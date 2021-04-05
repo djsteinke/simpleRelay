64ee9c3735aa1dc72308a815b3ddf21d76b44b1e
ds111g <ds111g@att.com>
2021-04-05 12:54:47 -0600

    add gpiozero

--- relay_led.py
+++ relay_led.py
@@ -5,14 +5,19 @@ from gpiozero import LED
 class RelayLED(object):
     def __init__(self, pin):
         self._delay = 1
-        self._led = LED(pin=pin,initial_value=None)
+        self._on = False
+        self._led = LED(pin=pin, initial_value=None)
 
     def set_delay(self, delay_in):
         self._delay = delay_in
 
     def toggle(self):
         self._led.toggle()
-        timer = threading.Timer(self._delay, self.toggle)
-        timer.start()
+        if not self._on:
+            timer = threading.Timer(self._delay, self.toggle)
+            timer.start()
+            self._on = True
+        else:
+            self._on = False
 
 
