3bf04ce526fd262a6883c3918d79384d5c0b036c
ds111g <ds111g@att.com>
2021-04-09 08:58:12 -0600

    host

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
         host_name = ip
     else:
         host_name = "localhost"
-    logger.info("app host_name[" + host_name + "]")
+    logger.info(f"app host_name[{host_name}]")
     # app.run(ssl_context='adhoc', host=host_name, port=1983)
     tof.start()
     app.run(host=host_name, port=port)
